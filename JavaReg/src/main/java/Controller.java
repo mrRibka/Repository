5e7ded1580ee17aa4b49f8017bbc1ca0b059713a
mrRibka <egorkondratov@gmail.com>
2021-09-01 21:55:23 +0300

    1st commit

---
+++ JavaReg/src/main/java/Controller.java
@@ -0,0 +1,13 @@
+public class Controller {
+
+    private final Model model;
+    private final View view;
+
+    public Controller(Model model, View view) {
+        this.model  = model;
+        this.view = view;
+    }
+     public void User(){
+
+     }
+}
